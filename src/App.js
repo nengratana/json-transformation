06daedcfff4a6a8c9b50fd562e594f02108afdac
Ratana Limnararat <neng.ratana@gmail.com>
2018-07-28 02:54:10 +0700

    Add control to input

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    input: "",
+    output: ""
+  };
+
+  handleInput = event => {
+    const input = event.target.value;
+    this.setState({ input });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>JSON Transformation</h1>
-        <h3>INPUT</h3>
-        <textarea className="input box" />
+        {this.state.input}
+        <textarea
+          className="input box"
+          value={this.state.input}
+          onChange={this.handleInput}
+        />
         <textarea className="output box" disabled />
       </div>
     );
